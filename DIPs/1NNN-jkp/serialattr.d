c196d4a1deba75cef19c75f5411184d98d8c3dcd
Jesse Phillips <Jesse.K.Phillips@gmail.com>
2019-12-14 10:40:28 -0800

    Show Serializer Selection

--- DIPs/1NNN-jkp/serialattr.d
+++ DIPs/1NNN-jkp/serialattr.d
@@ -1,20 +1,29 @@
 import std;
 
 struct DB{}
+struct WEB{}
 
-struct Ignore{}
+struct Ignore(T) {}
 
+struct SerializerDB(T) {}
+
+@SerializerDB!DB
 struct Example {
-    @Ignore
+    @Ignore!DB
     int a;
+    @Ignore!WEB
     int b;
 }
 
 void printAttributeProperties() {
-    Example e;
+    alias group = getUDAs!(Example, SerializerDB);
     static foreach(field; FieldNameTuple!(Example)) {
-        writeln(mixin("getUDAs!(Example." ~ field ~ ", Ignore).stringof"));
+        writeln(mixin("getUDAs!(Example." ~ field ~ ", Ignore!"~firstTemplateArg!group~").stringof"));
     }
+}
+
+string firstTemplateArg(alias t)() {
+    return TemplateArgsOf!t[0].stringof;
 }
 
 void main() {
